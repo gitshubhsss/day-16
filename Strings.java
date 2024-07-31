88998f78e61fc8cc7723ea352b37905d3fff646e
gitshubhsss <shubhamranjane16@gmail.com>
2024-07-31 22:56:43 +0530

    .\Strings.java

---
+++ Strings.java
@@ -0,0 +1,12 @@
+public class Strings {
+    public static void main(String[] args) {
+        
+        String name="shubham";
+        
+        for(int i=0;i<name.length();i++){
+            System.out.print(name.charAt(i));
+        }
+
+      
+    }
+}
